ee9016f656c5f422fa1d5ff3eb4d9bbd0822a452
Caio Souza <ocaiomoises@gmail.com>
2024-10-23 07:31:42 -0300

    done - this changes icons when clicked

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,15 @@
-const icons = document.querySelectorAll(".interaction-icons");
+const iconsScope = document.querySelectorAll(".interaction-icons");
+const plusIcon = document.querySelectorAll(".icon-plus");
+const minusIcon = document.querySelectorAll(".icon-minus");
 const contents = document.querySelectorAll(".content");
 
-icons.forEach((icon, index) => {
+iconsScope.forEach((icon, index) => {
     icon.addEventListener("click", () => {
+        // Changes the visibility of the content
         contents[index].classList.toggle("hidden");
+
+        // Changes the visibility of the icons
+        plusIcon[index].classList.toggle("hidden");
+        minusIcon[index].classList.toggle("hidden");
     });
 });
